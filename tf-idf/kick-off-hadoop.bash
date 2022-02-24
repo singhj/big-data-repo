8abbd90c0002365a56aaa0df0dc18ed5b9bb1729
singhj <j.singh@datathinks.org>
2022-02-24 20:18:38 +0000

    refining kick-off

--- tf-idf/kick-off-hadoop.bash
+++ tf-idf/kick-off-hadoop.bash
@@ -1,3 +1,5 @@
+#! /usr/bin/env bash
 wget https://storage.googleapis.com/jsingh-bigdata-public/inaugs.tar.gz
-hadoop fs -put /user/inputs/inaugs.tar.gz
+hadoop fs -mkdir /user/inputs/
+hadoop fs -put -f inaugs.tar.gz  /user/inputs/
 hadoop jar /usr/lib/hadoop/hadoop-streaming.jar -files mapper.py,reducer.py -mapper mapper.py -reducer reducer.py -input /user/inputs/inaugs.tar.gz -output /user/j_singh/inaugs
