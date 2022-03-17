c3e5dc75b880fc72e357c0d114c6bf818d01c63c
Jitendra Singh <singhj@github.com>
2022-03-16 21:06:52 -0400

    added spark-examples

---
+++ spark-examples/inaugural-speech-feeder.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import nltk
+import random
+import time
+
+nltk.download('inaugural')
+nltk.download('punkt')
+from nltk.corpus import inaugural
+fileids = nltk.corpus.inaugural.fileids()
+# print (fileids)
+
+def next_delay(self):
+    return random.normalvariate(self.delay[0], self.delay[1])
+
+for fileid in fileids:
+    sents = inaugural.sents(fileid)
+    for sent in sents:
+        print (fileid, sent)
+        minutes = 150
+        words = len(sent)
+        delay = 60.0 * words/150
+        # print (delay, words, words/delay)
+        time.sleep(delay)
+    # break
