c150bb30d93fc9140dfe8571ef1f03a20144fb83
singhj <j.singh@datathinks.org>
2022-03-23 17:56:41 +0000

    commented

--- spark-examples/quiz6-prep.py
+++ spark-examples/quiz6-prep.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 
 import os, sys
-import subprocess
 import traceback as tb
+
+# install pip if necessary
 os.system('sudo apt-get -y install python3-pip')
 
 try:
     import nltk
 except ImportError:
-    print ('nltk is not installed')
+    print ('nltk was not installed, installing...')
     os.system('pip3 install nltk')
 except AttributeError as exception:
-    # tb.print_stack()
-    # print ("AttibuteError")
     exception_type, exception_object, exception_traceback = sys.exc_info()
     foo = tb.extract_tb(exception_traceback)[-1]
-    print ('filename', foo.filename)
+    print ('Repairing file', foo.filename)
     filename = foo.filename
-    print('******', filename)
+
+    # This repair: https://github.com/nltk/nltk/commit/6428c9288a86658cb3d9a1e91816c4bcf162a6f0
     os.system(' '.join(['sudo sed -i',
                         's/regex.Pattern:/\\"regex.Pattern\\":/g',
                         filename]))
