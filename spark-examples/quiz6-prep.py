6487166c03f476852021c9bc682425db8a48aa6c
singhj <j.singh@datathinks.org>
2022-03-23 17:43:33 +0000

    repair casual.py

--- spark-examples/quiz6-prep.py
+++ spark-examples/quiz6-prep.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python3
 
-import os
-os.system('sudo apt-get install python3-pip')
+import os, sys
+import subprocess
+import traceback as tb
+os.system('sudo apt-get -y install python3-pip')
 
 try:
     import nltk
 except ImportError:
     print ('nltk is not installed')
     os.system('pip3 install nltk')
+except AttributeError as exception:
+    # tb.print_stack()
+    # print ("AttibuteError")
+    exception_type, exception_object, exception_traceback = sys.exc_info()
+    foo = tb.extract_tb(exception_traceback)[-1]
+    print ('filename', foo.filename)
+    filename = foo.filename
+    print('******', filename)
+    os.system(' '.join(['sudo sed -i',
+                        's/regex.Pattern:/\\"regex.Pattern\\":/g',
+                        filename]))
+
 finally:
     pass
 
-try:
-    import nltk
-except AttributeError:
-    print ("AttibuteError")
-    os.system('sudo sed -i', 's/regex.Pattern:/\"regex.Pattern\":/g','/opt/conda/default/lib/python3.8/site-packages/nltk/tokenize/casual.py')
-finally:
-    pass
+
