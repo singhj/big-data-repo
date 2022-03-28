2cd95ee484e4d7b2332cb87430187d1ad573416f
Jitendra Singh <singhj@github.com>
2022-03-28 11:04:18 -0400

    added click-feeder

---
+++ spark-examples/click-feeder.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from click_gen import *
+max_evs = 60           # Total number of events that will be generated. 
+                       # If you want to make it run forever, set this number to like a million
+
+n_senders = 6         # Number of senders
+n_queries = 6000      # How many different queries (or messages a sender can send)
+
+dispatcher = Dispatcher()
+sender_names = shuffle(['sndr%04d' % i for i in range(n_senders)])
+senders = [dispatcher.add_sender(Sender(sender_name, n_queries, 1.5, 0.5)) for sender_name in sender_names]
+
+for ev in dispatcher.launch():
+    print(ev, flush=True)
+    max_evs -= 1
+    if max_evs == 0:
+        break
+
+
