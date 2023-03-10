f0131f65880861f1c6d7605ff06a6cf55a6331ba
Jitendra Singh <singhj@github.com>
2023-03-10 15:26:44 -0500

    added clean_text.py

---
+++ clean_text.py
@@ -0,0 +1,8 @@
+import re
+def clean_text(text):
+    text = text.lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), ' ', text)
+    text = re.sub('[\d\n]', ' ', text)
+    text = ' '.join([w for w in text.split() if len(w)>3])
+    return text.strip()
