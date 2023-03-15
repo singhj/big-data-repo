5c712493ca3bc0f1a92d67d7a97ff0400f820a77
Jitendra Singh <singhj@github.com>
2023-03-15 11:54:04 -0400

    added filename to every line sent

--- spark-examples/inaugural-speech-feeder.py
+++ spark-examples/inaugural-speech-feeder.py
@@ -21,14 +21,16 @@ def random_delay(avg):
     return random.normalvariate(float(avg), 0.15*float(avg))
 
 for fileid in fileids:
+    # Sentences
     sents = inaugural.sents(fileid)
+    print (fileid, '#### #### ####', flush=True)
     for sent in sents:
-        print ('sent', ' '.join(sent))
+        # print ('sent', ' '.join(sent))
         rate = 150/60
         words = len(sent)
         delay = random_delay(words/rate)
-        print (fileid, f"{delay:5.2f}", ' '.join(sent), flush=True, file=sys.stderr)
-        print (fileid, (clean_text.clean(' '.join(sent))), flush=True)
-        # print (delay, words, words/delay, flush=True)
+        # uncomment to debug
+        # print (fileid, f"{delay:5.2f}", ' '.join(sent), flush=True, file=sys.stderr)
+        print (fileid, '|', clean_text.clean(' '.join(sent)), flush=True)
         time.sleep(delay)
     # break
