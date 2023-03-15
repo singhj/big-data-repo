e1a01c0e469f20895da07d21b8131866a6831dc9
Jitendra Singh <singhj@github.com>
2023-03-15 16:37:03 -0400

    amended network-wordcount

--- spark-examples/network-wordcount.sh
+++ spark-examples/network-wordcount.sh
@@ -3,5 +3,5 @@
 # Source: https://github.com/apache/spark/blob/v3.3.2/examples/src/main/python/streaming/network_wordcount.py
 # Created 3/15/2023 by jitendra.singh@tufts.edu
 #
-`which spark-submit` ./network_wordcount.py localhost 9999
+`which spark-submit` ~/big-data-repo/spark-examples/network-wordcount.py localhost 9999
 
