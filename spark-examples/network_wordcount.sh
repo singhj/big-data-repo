fa6ec3c92e4b51565e66bf00d0d6cbadc0c4ff45
Jitendra Singh <singhj@github.com>
2023-03-15 12:33:56 -0400

    added script for launching network_wordcount

---
+++ spark-examples/network_wordcount.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Source: https://github.com/apache/spark/blob/v3.3.2/examples/src/main/python/streaming/network_wordcount.py
+# Created 3/15/2023 by jitendra.singh@tufts.edu
+#
+`which spark-submit` ./network_wordcount.py localhost 9999`
+
