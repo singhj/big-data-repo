f71d37d06e03c3c84b3304109eb47db2b3eba32a
Jitendra Singh <singhj@github.com>
2024-03-13 22:09:58 -0400

    structured-streaming-related updates

--- spark-examples/network-wordcount.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Source: https://github.com/apache/spark/blob/v3.3.2/examples/src/main/python/streaming/network_wordcount.py
-# Created 3/15/2023 by jitendra.singh@tufts.edu
-#
-`which spark-submit` ~/big-data-repo/spark-examples/network-wordcount.py localhost 9999
-
---
+++ spark-examples/stream-wordcount.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Created 3/13/2024 by jitendra.singh@tufts.edu
+#
+`which spark-submit` /home/`whoami`/big-data-repo/spark-examples/structured_network_wordcount.py localhost 9999
+
